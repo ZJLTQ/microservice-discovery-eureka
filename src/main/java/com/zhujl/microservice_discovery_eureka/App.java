3fc40e19a2c6c8cc04fa1046eb3ec36fb529d506
zjltq <zhujilong110@163.com>
2016-09-14 16:56:04 +0800

    Signed-off-by: zjltq <zhujilong110@163.com>

---
+++ src/main/java/com/zhujl/microservice_discovery_eureka/App.java
@@ -0,0 +1,13 @@
+package com.zhujl.microservice_discovery_eureka;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
